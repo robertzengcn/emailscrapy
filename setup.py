4a07ba14f08ad929d55739b0a88d8da475ed4258
robert zeng <jianze_zeng@foxitsoftware.com>
2022-07-19 10:25:46 +0800

    remove usage module

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(name='Scrapyemail',
       long_description_content_type="text/markdown",
       author='Robert Zeng',
       author_email='zengjianze@gmail.com',
-      py_modules=['usage'],
       packages=['Scrapyemail'],
       package_dir={'examples': 'examples'},
       install_requires=requirements
